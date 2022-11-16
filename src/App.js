6efcbcb3375ea7ebadaeb7b137363f23ea981f64
Ishrat Jahan Easha <ishratjahaneasha239@gmail.com>
2022-11-16 12:32:14 +0600

    invitation added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HTMLFlipBook from "react-pageflip";
+import cover from "./images/cover.png";
+import P1Img from "./images/p1-img.png";
+import P1 from "./images/p1.png";
+import P2Img from "./images/p2-img.png";
+import P2 from "./images/p2.png";
+import "./styles.css";
+ 
 
-function App() {
+const pages = [P1Img, P1, P2Img, P2];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    
+      <h1>Easha & Sabbir</h1>
+
+      <div className="book">
+        <HTMLFlipBook className="flipBook"
+          width={500}
+          height={620}
+          // minWidth={200}
+          // maxWidth={500}
+          // minHeight={320}
+          // maxHeight={750}
+          size="fixed"
+          showCover={true}
+          maxShadowOpacity={0.5}
+          mobileScrollSupport={true}
+          style={{ background: "lavender" }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="demoPage">
+            <img src={cover} alt="cover" />
+          </div>
+          {pages.map((page, i) => (
+            <div className="demoPage" key={i}>
+              <img src={page} alt={i} 
+              />
+              
+            </div>
+          ))}
+        </HTMLFlipBook>
+      </div>
     </div>
   );
 }
-
-export default App;
